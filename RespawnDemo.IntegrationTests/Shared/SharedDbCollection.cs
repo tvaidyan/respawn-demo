f0d069aba79b7603344e040a78d6a63754baa409
Tom Vaidyan <tvaidyan@gmail.com>
2022-11-17 07:07:26 -0600

    remove unused usings

--- RespawnDemo.IntegrationTests/Shared/SharedDbCollection.cs
+++ RespawnDemo.IntegrationTests/Shared/SharedDbCollection.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace RespawnDemo.IntegrationTests.Shared
 {
     [CollectionDefinition("EmployeeDbCollection")]
-    public class SharedDbCollection : ICollectionFixture<DbFixture>
-    {
+public class SharedDbCollection : ICollectionFixture<DbFixture>
+{
 
-    }
+}
 }
